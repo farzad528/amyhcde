c36c1ca5106d4c5c4559286c7a8821fd0f3eb15b
Walison Theodoro <theodoro.walison@gmail.com>
2022-08-13 11:40:19 -0300

    fix: custom hook

--- hooks/useCountDown.js
+++ hooks/useCountDown.js
@@ -1,22 +1,21 @@
 import { useEffect, useState } from 'react'
 
 const useCountdown = (targetDate) => {
-  // const countDownDate = new Date(targetDate).getTime()
+  const countDownDate = new Date(targetDate).getTime()
 
-  // const [countDown, setCountDown] = useState(
-  //   countDownDate - new Date().getTime()
-  // )
+  const [countDown, setCountDown] = useState(
+    countDownDate - new Date().getTime()
+  )
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setCountDown(countDownDate - new Date().getTime())
-  //   }, 1000)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(countDownDate - new Date().getTime())
+    }, 1000)
 
-  //   return () => clearInterval(interval)
-  // }, [countDownDate])
+    return () => clearInterval(interval)
+  }, [countDownDate])
 
-  // return getReturnValues(countDown)
-  return getReturnValues(999999999999)
+  return getReturnValues(countDown)
 }
 
 const getReturnValues = (countDown) => {
