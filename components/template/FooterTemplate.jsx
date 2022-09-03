6c76115e87f117daad6ad8c6b5736a2e56cf0218
Walison Theodoro <theodoro.walison@gmail.com>
2022-09-03 10:54:55 -0300

    feat: add new home section

--- components/template/FooterTemplate.jsx
+++ components/template/FooterTemplate.jsx
@@ -6,36 +6,6 @@ import GiftIcon from '../icons/GiftIcon'
 
 const FooterTemplate = () => (
   <footer className={styles.footerTemplate}>
-    <div className={styles.subscribeHeader}>
-      <a
-        className={styles.mediaWrapper}
-        href='https://www.facebook.com/amyforhcde'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <FacebookIcon />
-        <span>Facebook</span>
-      </a>
-      <a
-        className={styles.mediaWrapper}
-        href='mailto: amyforhcde@gmail.com'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <EmailIcon />
-        <span>Email</span>
-      </a>
-      <a
-        className={styles.mediaWrapper}
-        href='https://secure.actblue.com/donate/amy-hinojosa-for-hcde'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <GiftIcon />
-        <span>Donate</span>
-      </a>
-    </div>
-
     <div className={styles.subscribeFooter}>
       <p>Made with ❤️ by Amy’s supporters.️</p>
     </div>
--- components/template/SubscribeTemplate.jsx
+++ components/template/SubscribeTemplate.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
 import styles from './SubscribeTemplate.module.css'
 import { useCountdown } from '../../hooks/useCountDown'
 import FacebookIcon from '../icons/FacebookIcon'
 import TweeterIcon from '../icons/TweeterIcon'
 import EmailIcon from '../icons/EmailIcon'
 import GiftIcon from '../icons/GiftIcon'
+import AmyHero from '../../assets/amyHero.png'
 
 const SubscribeTemplate = ({ onNavClick }) => {
   // const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000
@@ -13,13 +15,14 @@ const SubscribeTemplate = ({ onNavClick }) => {
 
   const EVENT = new Date('08 November 2022 07:00 CST').getTime()
 
+  const heroImageScale = 0.8
+
   const countDown = useCountdown(EVENT)
   return (
     <main className={styles.subscribeTemplateContainer}>
+      {/* header */}
       <div className={styles.subscribeHeader}>
-        <p onClick={() => console.log(countDown)}>
-          Made with ❤️ by Amy’s supporters.️
-        </p>
+        <p className={styles.subscribeHeaderLabel}>AmyHinojosa</p>
 
         <div>
           <p className={styles.navLink} onClick={() => onNavClick('about')}>
@@ -38,77 +41,94 @@ const SubscribeTemplate = ({ onNavClick }) => {
             What’s at Stake?
           </p>
         </div>
-      </div>
 
-      <div className={styles.logoWrapper}>
-        <img src='images/logo.png' alt='amy hinojosa logo' />
-      </div>
-
-      <div className={styles.counterWrapper}>
-        <h2>VOTING STARTS:</h2>
-
-        <div>
-          <div className={styles.counterNumberDay}>
-            <p className={styles.counterNumber}>{countDown.days}</p>
-            <p className={styles.counterName}>Days</p>
-          </div>
-          <span>:</span>
-          <div className={styles.counterNumberDay}>
-            <p className={styles.counterNumber}>{countDown.hours}</p>
-            <p className={styles.counterName}>Hours</p>
-          </div>
-          <span>:</span>
-          <div className={styles.counterNumberDay}>
-            <p className={styles.counterNumber}>{countDown.minutes}</p>
-            <p className={styles.counterName}>Minutes</p>
-          </div>
-          <span>:</span>
-          <div className={styles.counterNumberDay}>
-            <p className={styles.counterNumber}>{countDown.seconds}</p>
-            <p className={styles.counterName}>Seconds</p>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.subscribeContent}>
-        <p className={styles.messageWrapper}>
-          Support Amy Flores Hinojosa for your Harris County Department of
-          Education Chair
-        </p>
-      </div>
-
-      <div className={styles.mediasWrapper}>
-        <a
-          className={styles.mediaWrapper}
-          href='https://www.facebook.com/amyforhcde'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <FacebookIcon />
-          <span>Facebook</span>
-        </a>
-        {/* <a className={styles.mediaWrapper} href='https://google.com'>
+        <div className={styles.mediasWrapper}>
+          <a
+            className={styles.mediaWrapper}
+            href='https://www.facebook.com/amyforhcde'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FacebookIcon />
+          </a>
+          {/* <a className={styles.mediaWrapper} href='https://google.com'>
           <TweeterIcon />
           <span>Tweet</span>
         </a> */}
-        <a
-          className={styles.mediaWrapper}
-          href='mailto: amyforhcde@gmail.com'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <EmailIcon />
-          <span>Email</span>
-        </a>
-        <a
-          className={styles.mediaWrapper}
-          href='https://secure.actblue.com/donate/amy-hinojosa-for-hcde'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <GiftIcon />
-          <span>Donate</span>
-        </a>
+          <a
+            className={styles.mediaWrapper}
+            href='mailto: amyforhcde@gmail.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <EmailIcon />
+          </a>
+          <a
+            className={styles.mediaWrapper}
+            href='https://secure.actblue.com/donate/amy-hinojosa-for-hcde'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <GiftIcon />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.mainContent}>
+        {/* LEFT */}
+        <div className={styles.leftSide}>
+          <div className={styles.causeText}>
+            <h2>Elevate Students, Elevate Families, Elevate Generations</h2>
+            <p>
+              Support Amy Hinojosa for your Harris County School Trustee,
+              Position 1 Precinct 2
+            </p>
+            <a
+              className={styles.donateButton}
+              href='https://secure.actblue.com/donate/amy-hinojosa-for-hcde'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Donate
+            </a>
+          </div>
+
+          <div className={styles.counterWrapper}>
+            <h2>VOTING STARTS:</h2>
+
+            <div>
+              <div className={styles.counterNumberDay}>
+                <p className={styles.counterNumber}>{countDown.days}</p>
+                <p className={styles.counterName}>Days</p>
+              </div>
+              <span>:</span>
+              <div className={styles.counterNumberDay}>
+                <p className={styles.counterNumber}>{countDown.hours}</p>
+                <p className={styles.counterName}>Hours</p>
+              </div>
+              <span>:</span>
+              <div className={styles.counterNumberDay}>
+                <p className={styles.counterNumber}>{countDown.minutes}</p>
+                <p className={styles.counterName}>Minutes</p>
+              </div>
+              <span>:</span>
+              <div className={styles.counterNumberDay}>
+                <p className={styles.counterNumber}>{countDown.seconds}</p>
+                <p className={styles.counterName}>Seconds</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* RIGHT */}
+        <div className={styles.rightSide}>
+          <Image
+            src={AmyHero}
+            height={789 * heroImageScale}
+            width={354 * heroImageScale}
+            className={styles.nextImage}
+          />
+        </div>
       </div>
     </main>
   )
