577ac5ed5b0002597cb074b5f8ffe410b7eaa712
Farzad <fsunavala@microsoft.com>
2022-09-03 12:25:50 -0500

    fillOpacity

--- components/icons/BackIcon.jsx
+++ components/icons/BackIcon.jsx
@@ -9,7 +9,7 @@ const BackIcon = (style = 'black' ) => (
     <path
       d="M16 7H3.83L9.42 1.41L8 0L0 8L8 16L9.41 14.59L3.83 9H16V7Z"
       fill="black"
-      fill-opacity="0.54"
+      fillOpacity="0.54"
     />
   </svg>
 );
