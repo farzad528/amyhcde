38adbaa384c503e9d2d23e3a13ce6f9c3e49afd8
Farzad <fsunavala@microsoft.com>
2022-09-06 12:47:56 -0500

    footer and h3 color fix

--- components/template/FooterTemplate.jsx
+++ components/template/FooterTemplate.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import styles from './FooterTemplate.module.css'
-import FacebookIcon from '../icons/FacebookIcon'
-import EmailIcon from '../icons/EmailIcon'
-import GiftIcon from '../icons/GiftIcon'
+
 
 const FooterTemplate = () => (
   <footer className={styles.footerTemplate}>
